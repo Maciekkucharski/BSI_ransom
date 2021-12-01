dca35a02a7517a10d656cc031971edb4d252b53b
KonstantyFlint <43811569+KonstantyFlint@users.noreply.github.com>
2021-12-01 15:10:54 +0100

    added twofish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from encryptors import *
+import tf
 encryption_key = b"0123456789123456"
 encryption_key_des = b'12345678'
 
@@ -9,3 +10,7 @@ input()
 encrypt_files(encrypt_DES, encryption_key_des)
 input()
 decrypt_files(decrypt_DES, encryption_key_des)
+input()
+tf_encrypt("twofish\\plain.txt", "twofish\\encoded.txt", '1337')
+input()
+tf_decrypt("twofish\\encoded.txt", "twofish\\plain2.txt", '1337')
