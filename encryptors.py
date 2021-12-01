b56a063b699e3ef08fef123507e7a3a3856bde9b
unknown <m.kucha@wp.pl>
2021-12-01 19:43:23 +0100

    finished

--- encryptors.py
+++ encryptors.py
@@ -7,6 +7,16 @@ tag_dict = dict()
 
 
 def encrypt_AES(msg, key):
+    '''Encrypts file with aes
+    ARGS:
+        msg: text to encrypt
+        key: key used to encode
+    Returns:
+        Tupple of:
+            bytes: random bits that will allow to decode the encoded phrase
+            bytes: encoded phrase
+            bytes: used to verify is decoded properly
+    '''
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
     cipher_text, tag = cipher.encrypt_and_digest(msg.encode('ascii'))
@@ -14,6 +24,17 @@ def encrypt_AES(msg, key):
 
 
 def decrypt_AES(nonce, ciphertext, tag, key):
+    '''Decrypts file with aes
+        ARGS:
+            nonce: random bits that will allow to decode the encoded phrase
+            cipher_text: encoded phrase
+            tag: used to verify is decoded properly
+            key: key used to encode
+        Returns:
+            str: decoded string
+            or
+            False: if not decoded right
+    '''
     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
     plaintext = cipher.decrypt(ciphertext)
     try:
@@ -24,6 +45,16 @@ def decrypt_AES(nonce, ciphertext, tag, key):
 
 
 def encrypt_DES(msg, key):
+    '''Encrypts file with des
+        ARGS:
+            msg: text to encrypt
+            key: key used to encode
+        Returns:
+            Tupple of:
+                bytes: random bits that will allow to decode the encoded phrase
+                bytes: encoded phrase
+                bytes: used to verify is decoded properly
+    '''
     cipher = DES.new(key, DES.MODE_EAX)
     nonce = cipher.nonce
     cipher_text, tag = cipher.encrypt_and_digest(msg.encode('ascii'))
@@ -31,6 +62,17 @@ def encrypt_DES(msg, key):
 
 
 def decrypt_DES(nonce, ciphertext, tag, key):
+    '''Decrypts file with des
+        ARGS:
+            nonce: random bits that will allow to decode the encoded phrase
+            cipher_text: encoded phrase
+            tag: used to verify is decoded properly
+            key: key used to encode
+        Returns:
+            str: decoded string
+            or
+            False: if not decoded right
+    '''
     cipher = DES.new(key, DES.MODE_EAX, nonce=nonce)
     plaintext = cipher.decrypt(ciphertext)
     try:
@@ -41,6 +83,12 @@ def decrypt_DES(nonce, ciphertext, tag, key):
 
 
 def encrypt_files(encryption_method, key):
+    '''Encrypts files with chosen method
+        ARGS:
+            encryption_method: method used to encrypt
+            key: key used to encrypt
+        Returns:
+    '''
     for file in glob("AES/to_encrypt/*.txt"):
         with open(file, "r") as f:
             nonce, cipher_text, tag = encryption_method(f.read(), key)
@@ -52,6 +100,12 @@ def encrypt_files(encryption_method, key):
 
 
 def decrypt_files(decryption_method, key):
+    '''Decrypts files with chosen method
+        ARGS:
+            encryption_method: method used to decrypt
+            key: key used to encrypt
+        Returns:
+    '''
     for file in glob("AES/to_encrypt/*.txt"):
         with open(file, "r+") as f:
             nonce = nonce_dict[file]
--- main.py
+++ main.py
@@ -1,16 +1,55 @@
 from encryptors import *
+
 import tf
-encryption_key = b"0123456789123456"
+# input()
+# tf_encrypt("twofish\\plain.txt", "twofish\\encoded.txt", '1337')
+# input()
+# tf_decrypt("twofish\\encoded.txt", "twofish\\plain2.txt", '1337')
+
+
+from encryptors import *
+
+nonce_dict = dict()
+cipher_dict = dict()
+tag_dict = dict()
+
+encryption_key_aes = b"0123456789123456"
 encryption_key_des = b'12345678'
 
-# encrypt_files(encrypt_AES, encryption_key)
-# input()
-# decrypt_files(decrypt_AES, encryption_key)
-# input()
-# encrypt_files(encrypt_DES, encryption_key_des)
-# input()
-# decrypt_files(decrypt_DES, encryption_key_des)
-# input()
-tf_encrypt("twofish\\plain.txt", "twofish\\encoded.txt", '1337')
-input()
-tf_decrypt("twofish\\encoded.txt", "twofish\\plain2.txt", '1337')
+choice_dict = {
+    "1": encrypt_files,
+    "2": decrypt_files,
+}
+choice_dict2 = {
+    "1": encrypt_AES,
+    "2": encrypt_DES,
+    "3": decrypt_AES,
+    "4": decrypt_DES,
+}
+key_dict = {
+    "1": encryption_key_aes,
+    "2": encryption_key_des,
+    "3": encryption_key_aes,
+    "4": encryption_key_des,
+}
+
+first_choice = 0
+second_choice = 0
+while True:
+    while True:
+        first_choice = input("1: encode 2: decode, 0: exit")
+        if first_choice in ['0', '1', '2']:
+            break
+    if first_choice == '0':
+        quit()
+    if first_choice == '1':
+        while True:
+            second_choice = input("1: AES 2: DES")
+            if second_choice in ['1', '2']:
+                break
+    else:
+        while True:
+            second_choice = input("3: AES 4: DES")
+            if second_choice in ['3', '4']:
+                break
+    choice_dict[first_choice](choice_dict2[second_choice], key_dict[second_choice])
